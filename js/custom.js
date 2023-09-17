d21c2764416c4ac92005ad998f2b6f5fd1c8a1fe
mostafageo2009 <sasapopsasa@gmail.com>
2023-09-18 00:42:59 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+/*
+	Rocket Internet by TEMPLATE STOCK
+    templatestock.co @templatestock
+*/
+
+/* Header Full Screen */
+$(document).ready(function() {
+	$('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+	$(window).resize(function(){
+        $('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+    });
+
+});
+
+setTimeout(function () {
+    $('.loader_bg').fadeToggle();
+}, 1500);
